9fb6d9a35579142fe71ac7e48215738d550c5be3
cdowell09 <cdowell09@gmail.com>
2024-07-01 20:44:52 -0700

    python scripts from cli

--- script.py
+++ script.py
@@ -10,15 +10,3 @@ def hello(name: str):
 
 if __name__ == "__main__":
     app()
-
-
-def main(
-    name: str,
-    age: int = typer.Option(18, "--age", "-a"),
-    is_student: bool = typer.Option(False, "--student/--not-student"),
-):
-    typer.echo(f"Name: {name}, Age: {age}, Student: {is_student}")
-
-
-if __name__ == "__main__":
-    typer.run(main)
